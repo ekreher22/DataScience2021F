50a8bb086a63ac7e736f54ba68535e1cae6206f3
Elaina <ekreher22@penguinhall.org>
2021-10-17 14:21:18 -0400

    all unit 2 work

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,10 +1,43 @@
-def my_func (first):
-    p = prefix()
-    last = input("Hi " + p + first + "! Please enter last name: ")
-    print("Nice to meet you " + first + " " + last + "!")
+import random 
+# choosing random word
+word_options = ['apple', 'banana', 'cherry']
+word = random.choice(word_options) 
 
-def prefix():
-    return "Mr. "
 
-my_func("Elaina")
+def play_hangman():
+    print ('Welcome to Hangman!')
+    print ('The secret word has ' + str(len(word)) + ' letters and the theme is fruit!') 
+    word_letters = set(word) 
+    used_letters = set()
+    num_of_guesses = 0
+    max_num_of_guesses = 10
+    gameover = False
+    while (num_of_guesses < max_num_of_guesses) & (gameover == False):
+        user_guess = input ('Enter guess:').lower()
+        if user_guess in word_letters:
+            print('Yes, "' + str(user_guess) + '" is in the word!')
+            word_letters.remove(user_guess)
+            used_letters.add(user_guess)
+            num_of_guesses += 1
+        elif user_guess in used_letters:
+            print('You already guessed that letter')
+        else:
+            print('Not one of the letters!')
+            used_letters.add(user_guess)
+            num_of_guesses += 1
+        if word_letters == set():
+            print('You guessed the word!')
+            gameover = True   
+        else:
+            displayWord = [letter if letter in used_letters else '*' for letter in word]
+            print('So far you have ' + str(', '.join(displayWord)))
+            print('You have guessed:\n' +  str(used_letters))
+            print('You have ' + str((max_num_of_guesses - num_of_guesses)) +' guesses left.')
+    print('the game is over')
+  
+play_hangman()
 
+
+
+
+    
